0ae689efb76e9f8676fa2b99f7d9d9da2efcfe28
Ushie01 <abugodwinaj@gmail.com>
2023-06-08 04:34:32 +0100

    update: change of cors origin

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
 
 app.use(cors({
-  // origin: 'http://localhost:3000'
-   origin: 'https://vehicle-registration-app.vercel.app'
-}));
+  origin: 'https://vehicle-registration-app.vercel.app'
+}))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
